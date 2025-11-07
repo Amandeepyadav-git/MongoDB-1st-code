5971e70828bb9d4f8f5723c5c7d2cbde91927012
Amandeep Yadav <amandeepyadav0704@gmail.com>
2025-11-07 22:50:27 +0530

    Updated the file with new changes

--- app.js
+++ app.js
@@ -9,12 +9,29 @@ app.get("/",(req,res)=>{
 
 app.get("/create", async(req,res)=>{
   let createuser = await userModel.create({
-    name:"Amandeep1",
-    email:"Amandeep@gmail.com",
+    name:"Amandeep New",
+    email:"Amandeepnew@gmail.com",
     username: "Aman"
   })
   
   res.send(createuser);
+})
+
+app.get("/update",async (req,res)=>{
+
+let updated = await userModel.findOneAndUpdate({username: "Aman"}, {username: "Amandeep Yadav"}, {new:true})
+
+  res.send(updated);
+})
+
+app.get("/read",async (req,res)=>{
+ let users = await userModel.find({name:"Amandeep"});
+ res.send(users);
+});
+
+app.get("/delete", async (req,res)=>{
+ let deleteD = await userModel.deleteOne({name:"Amandeep New"});
+ res.send(deleteD);
 })
 
 app.listen(3000);
--- usermodel.js
+++ usermodel.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(`mongodb://127.0.0.1:27017/mongoPractice`)
-.then(()=>{consle.log("Mongodb connected succesfully")})
+.then(()=>{console.log("Mongodb connected succesfully")})
 .catch((err)=>{console.log("you got an error",err)})
 
 const userSchema = mongoose.Schema({
     name : String,
     username: String,
-    email: String
+    email: String,
+    address:String
 });
 
 module.exports = mongoose.model("user", userSchema);
